c16a1fd01aa616e03abd3c713b14781ed415d470
hsmcleod <hsmcleod@valdosta.edu>
2025-08-20 16:34:36 -0400

    Create inchestocm.c

---
+++ inchestocm.c
@@ -0,0 +1,14 @@
+/* Takes the side of a square in inches as input and computes and prints out the area of the square
+in  cm^2 as the output with 2 decimal places */
+
+#include <stdio.h>
+#define SCALE_FACTOR 2.54f
+
+int main(void){
+  float inches, centimeters, area;
+  printf("Enter side length in inches: \n");
+  scanf("%f", &inches);
+  centimeters = inches * SCALE_FACTOR;
+  printf("Area in cm^2: %.2f\n, area);
+  return 0;
+}
